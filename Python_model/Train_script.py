ce6332f5e01e03379d938f13c9e48b8abfc04f11
TropicaPenguin <55035872+tropicapenguin@users.noreply.github.com>
2023-10-10 12:48:18 -0500

    Create Train_script.py

---
+++ Python_model/Train_script.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import numpy as np
+import pathlib
+import datetime
+
+# printout versions
+print(f"Tensor Flow Version: {tf.__version__}")
+print(f"numpy Version: {np.version.version}")
+
+# Raw Dataset Directory
+data_dir = pathlib.Path("./flower_photos")
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+# classnames in the dataset specified
+CLASS_NAMES = np.array([item.name for item in data_dir.glob('*') if item.name != "LICENSE.txt" ])
+print(CLASS_NAMES)
+# print length of class names
+output_class_units = len(CLASS_NAMES)
+print(output_class_units)
+
+# Shape of inputs to NN Model
+BATCH_SIZE = 32             # Can be of size 2^n, but not restricted to. for the better utilization of memory
+IMG_HEIGHT = 227            # input Shape required by the model
+IMG_WIDTH = 227             # input Shape required by the model
+STEPS_PER_EPOCH = np.ceil(image_count/BATCH_SIZE)
+
+# Rescalingthe pixel values from 0~255 to 0~1 For RGB Channels of the image.
+image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+# training_data for model training
+train_data_gen = image_generator.flow_from_directory(directory=str(data_dir),
+                                                     batch_size=BATCH_SIZE,
+                                                     shuffle=True,
+                                                     target_size=(IMG_HEIGHT, IMG_WIDTH), #Resizing the raw dataset
+                                                     classes = list(CLASS_NAMES))
+
+# Specifying the optimizer, Loss function for optimization & Metrics to be displayed
+model.compile(optimizer='sgd', loss="categorical_crossentropy", metrics=['accuracy'])
+
+# Summarizing the model architecture and printing it out
+model.summary()
+
+# callbacks at training
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self,epoch,logs={}):
+        if (logs.get("accuracy")==1.00 and logs.get("loss")<0.03):
+            print("\nReached 100% accuracy so stopping training")
+            self.model.stop_training =True
+callbacks = myCallback()
+
+# TensorBoard.dev Visuals
+log_dir="logs\\fit\\" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+# Training the Model
+history = model.fit(
+      train_data_gen,
+      steps_per_epoch=STEPS_PER_EPOCH,
+      epochs=50,
+      callbacks=[tensorboard_callback,callbacks])
+
+# Saving the model
+model.save('AlexNet_saved_model/')
